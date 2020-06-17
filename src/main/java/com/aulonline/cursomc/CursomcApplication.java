07dd3d07982006968a1fe87f71cdee94f662d3be
Gustavo Valverde <gustavo.valverde5@gmail.com>
2020-06-17 20:23:37 -0300

    Criando operação de instanciação de Categorias durante inicialização do spring

--- src/main/java/com/aulonline/cursomc/CursomcApplication.java
+++ src/main/java/com/aulonline/cursomc/CursomcApplication.java
@@ -1,13 +1,29 @@
 package com.aulonline.cursomc;
 
+import com.aulonline.cursomc.dominio.Categoria;
+import com.aulonline.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria informatica = new Categoria(null, "Informática");
+        Categoria escritorio = new Categoria(null, "Escritório");
+
+        categoriaRepository.saveAll(Arrays.asList(informatica, escritorio));
+    }
 }
